ad52864af6e02f32e09cd44cfedca4f0f86f5718
Slava <slavaserdyukov@.com>
2024-11-03 15:58:38 +0100

    finaly

--- ya_note/notes/common.py
+++ ya_note/notes/common.py
@@ -1,17 +1,13 @@
-from http import HTTPStatus
-
-
 WARNING = "Это предупреждение"
 NOTES_ADD = "/notes/add/"
 NOTES_EDIT = "/notes/edit/"
+NOTES_LIST = "/notes/list/"
 NOTE_SLUG = "slug-note"
 
 
 def slugify(title):
-    """Превращает строку в slug, пригодный для URL."""
     return title.lower().replace(' ', '-')
 
 
 class BaseTestCase:
-    """Базовый класс для тестов."""
     pass
